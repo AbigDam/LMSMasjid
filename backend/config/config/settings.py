ee223636b0e1ed922ba2636daa1e1c37574778c8
YHaraziii <125395183+YHaraziii@users.noreply.github.com>
2026-07-02 11:50:06 -0400

    Updated settings to connect render to the monorepo
    Updated SECRET_KEY to be fetched from environment variables and modified database connection settings for production.

--- backend/config/config/settings.py
+++ backend/config/config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sfgzi5f7zv7=+o7zzq6c4_*@7sdj9zgv_fwhuu3(tbw+$tgg2t'
+
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-sfgzi5f7zv7=+o7zzq6c4_*@7sdj9zgv_fwhuu3(tbw+$tgg2t"
+)
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    ".onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
@@ -87,9 +97,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'postgres',
+        'USER': 'postgres.utahestzgdrjmsthjvds',
         'PASSWORD': 'rkcpB4fm8x1jXfw0',
-        'HOST': 'db.utahestzgdrjmsthjvds.supabase.co',
+        'HOST': 'aws-1-us-west-2.pooler.supabase.com',
         'PORT': '5432',
     }
 }
@@ -130,3 +140,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
